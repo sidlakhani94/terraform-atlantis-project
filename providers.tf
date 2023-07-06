ae51d6dc5fa32cc1377f30a97635011ebb8e485b
Siddharth Lakhani <57395308+sidlakhani94@users.noreply.github.com>
2023-07-06 13:41:15 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
 # Terraform Block
 terraform {
-  required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
+ # required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+ # required_providers {
+ #   aws = {
+ #     source  = "hashicorp/aws"
+ #     version = "~> 3.0"
+ #   }
+ # }
   backend "s3" {
     bucket  = "terraform-remote-state-ec2"
     key     = "dev/terraform-action-ec2/terraform.tfstate"
