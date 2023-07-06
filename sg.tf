81d980ad1ba783d3f15d4e8188d47bd91c7e26eb
Siddharth Lakhani <57395308+sidlakhani94@users.noreply.github.com>
2023-07-06 12:50:45 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -12,6 +12,15 @@ resource "aws_security_group" "my-sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
